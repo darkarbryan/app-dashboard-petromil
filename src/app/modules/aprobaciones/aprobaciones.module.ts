6bafb447978d30b576bd52fd0552d0a084ae8b5a
bryan valderrama <bryan_del_95@hotmail.com>
2024-05-21 22:59:09 -0500

    ADD component overview para modulo aprobaciones. ADD routing para modulo aprobaciones

--- src/app/modules/aprobaciones/aprobaciones.module.ts
+++ src/app/modules/aprobaciones/aprobaciones.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { OverviewComponent } from './components/overview/overview.component';
+import { AprobacionesRoutingModule } from './aprobaciones-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OverviewComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AprobacionesRoutingModule
   ]
 })
 export class AprobacionesModule { }
