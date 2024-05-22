cfdf184da0729c3da03f92dc08eb408141bd12c5
bryan valderrama <bryan_del_95@hotmail.com>
2024-05-21 23:07:03 -0500

    ADD component overview para modulo inversiones. ADD routing para modulo inversiones

--- src/app/modules/inversiones/inversiones.module.ts
+++ src/app/modules/inversiones/inversiones.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { OverviewComponent } from './components/overview/overview.component';
+import { InversionesRoutingModule } from './inversiones-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OverviewComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    InversionesRoutingModule
   ]
 })
 export class InversionesModule { }
