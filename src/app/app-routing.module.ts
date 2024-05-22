412150ecfb9d37c3646a623821ae8e663db47dfa
bryan valderrama <bryan_del_95@hotmail.com>
2024-05-21 22:50:43 -0500

    UPDATE routing para app.component. Se agrego las rutas de lazy loading para los modulos desde app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('@modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('@modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'config',
+    loadChildren: () => import('@modules/config/config.module').then(m => m.ConfigModule)
+  },
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    pathMatch: 'prefix',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
