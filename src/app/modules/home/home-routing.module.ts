f6e3673147ce6cbfb3fd28f553bec43c5f9db17e
bryan valderrama <bryan_del_95@hotmail.com>
2024-05-21 23:27:44 -0500

    ADD component overview para modulo home. ADD routing para modulo home

---
+++ src/app/modules/home/home-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from '@modules/home/components/overview/overview.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: OverviewComponent
+    },
+    {
+        path: 'inversiones',
+        loadChildren: () => import('@modules/inversiones/inversiones.module').then(m => m.InversionesModule)
+    },
+    {
+        path: 'cartera',
+        loadChildren: () => import('@modules/cartera/cartera.module').then(m => m.CarteraModule)
+    },
+    {
+        path: 'aprobaciones',
+        loadChildren: () => import('@modules/aprobaciones/aprobaciones.module').then(m => m.AprobacionesModule)
+    },
+    {
+        path: '**',
+        pathMatch: 'prefix',
+        redirectTo: '',
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class HomeRoutingModule {}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { OverviewComponent } from './components/overview/overview.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OverviewComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
